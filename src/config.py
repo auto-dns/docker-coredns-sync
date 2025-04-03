5202bbf4b34358798b8c81c0d4a0d461892465d4
Steven Carroll <stevencarroll90@gmail.com>
2025-04-03 01:49:35 +0000

    Getting etcd registering and deregistering updated

--- src/config.py
+++ src/config.py
@@ -1,10 +1,12 @@
 import os
 
+# General
 HOST_IP = os.getenv("HOST_IP", "127.0.0.1")
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-REDIS_PORT = os.getenv("REDIS_PORT", 6379)
-REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
-REDIS_PREFIX = os.getenv("REDIS_PREFIX", "_dns:")
 HOSTNAME = os.getenv("HOSTNAME", "your-hostname")
-LABEL_PREFIX = os.getenv("LABEL_PREFIX", "coredns")
+DOCKER_LABEL_PREFIX = os.getenv("DOCKER_LABEL_PREFIX", "coredns")
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+
+# etcd
+ETCD_HOST = os.getenv("ETCD_HOST", "localhost")
+ETCD_PORT = int(os.getenv("ETCD_PORT", 2379))
+ETCD_PATH_PREFIX = os.getenv("ETCD_PATH_PREFIX", "/skydns")
