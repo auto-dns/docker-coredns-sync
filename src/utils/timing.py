d467a1dc38d20efd8e6e6a7d878ec200d2e0a614
Steve Carroll <stevencarroll90@gmail.com>
2025-04-06 21:09:18 +0000

    Adding container get retry logic

---
+++ src/utils/timing.py
@@ -0,0 +1,26 @@
+# src/utils/timing.py
+import time
+import functools
+from typing import Callable, Any
+
+
+def retry(retries: int = 3, delay: float = 0.5, backoff = False, backoff_ratio = 2, exceptions: tuple = (Exception,), logger_func: Callable[[str], None] = print):
+	"""
+	A simple retry decorator with delay and optional logger.
+	"""
+	def decorator(func: Callable) -> Callable:
+		@functools.wraps(func)
+		def wrapper(*args, **kwargs) -> Any:
+			current_delay = delay
+			for attempt in range(1, retries + 1):
+				try:
+					return func(*args, **kwargs)
+				except exceptions as e:
+					if attempt == retries:
+						raise
+					logger_func(f"[retry] Attempt {attempt} failed: {e}. Retrying in {delay}s...")
+					time.sleep(current_delay)
+					if backoff:
+						current_delay *= backoff_ratio
+		return wrapper
+	return decorator
