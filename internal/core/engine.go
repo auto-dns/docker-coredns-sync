92ab63355375b1d00206e65116e924028048e7bf
Steven Carroll <stevencarroll90@gmail.com>
2025-04-16 02:40:08 +0000

    Adding startup loop

--- internal/core/engine.go
+++ internal/core/engine.go
@@ -29,9 +29,34 @@ func NewSyncEngine(logger zerolog.Logger, cfg *config.AppConfig, watcher DockerW
 	}
 }
 
-// TODO: improvement - aggregate list of currently running containers at startup
-// Accumulate list of events to process as soon as we're done handling the startup running containers
-// Don't reconcile state until we've finished with all that initialization
+func (se *SyncEngine) prepopulateState(ctx context.Context) error {
+	// You likely need a Docker client call to list current containers.
+	containers, err := se.watcher.ListRunningContainers(ctx)
+	if err != nil {
+		return err
+	}
+	for _, container := range containers {
+		// Convert container details to a ContainerEvent.
+		evt := ContainerEvent{
+			ID:      container.ID,
+			Name:    container.Name,
+			Status:  container.Status,
+			Created: container.Created,
+			Labels:  container.Labels,
+		}
+		// Build record intents for this container.
+		intents, err := GetContainerRecordIntents(evt, se.cfg, se.logger)
+		if err != nil {
+			se.logger.Error().Err(err).Msg("Error building record intents during prepopulation")
+			continue // or return err, depending on desired behavior
+		}
+		if len(intents) > 0 {
+			se.stateTracker.Upsert(evt.ID, evt.Name, evt.Created, intents, "running")
+			se.logger.Info().Msgf("Prepopulated state for container %s", evt.ID)
+		}
+	}
+	return nil
+}
 
 func (se *SyncEngine) handleEvent(evt ContainerEvent) {
 	if evt.ID == "" {
@@ -57,10 +82,15 @@ func (se *SyncEngine) handleEvent(evt ContainerEvent) {
 func (se *SyncEngine) Run(ctx context.Context) error {
 	se.logger.Info().Msg("SyncEngine starting")
 
-	// Subscribe to Docker events (assume this is done elsewhere)
+	// Step 1: Subscribe to Docker events (assume this is done elsewhere)
 	eventCh, err := se.watcher.Subscribe(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to subscribe to Docker events: %w", err)
+	}
+
+	// Step 2: Prepopulate state by listing running containers.
+	if err := se.prepopulateState(ctx); err != nil {
+		se.logger.Error().Err(err).Msg("Error during state prepopulation")
 	}
 
 	// Launch a goroutine to process incoming events and update the state tracker.
