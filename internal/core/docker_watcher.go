d23a4e586aed209cdbe7477e634ae7aa1d35b1a4
Steven Carroll <stevencarroll90@gmail.com>
2025-04-16 02:40:08 +0000

    Adding startup loop

--- internal/core/docker_watcher.go
+++ internal/core/docker_watcher.go
@@ -8,5 +8,6 @@ import (
 type DockerWatcher interface {
 	// Subscribe starts watching for events and returns a read-only channel.
 	Subscribe(ctx context.Context) (<-chan ContainerEvent, error)
+	ListRunningContainers(ctx context.Context) ([]ContainerEvent, error)
 	Stop() // used for cleanup
 }
